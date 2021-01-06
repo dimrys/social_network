0ea58308c1b655406ce1ab84171bd060a03037a5
dimrys <akkauntdlyraboti@gmail.com>
2021-01-06 20:34:51 +0300

    init commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import s from './Header.module.css'
+
+
+const Header = () => {
+    return(
+        <header className={s.header}>
+            <h1>dfgdfg</h1>
+            <img src="https://i.pinimg.com/originals/3f/3d/d9/3f3dd9219f7bb1c9617cf4f154b70383.jpg"/>
+        </header>
+    )
+}
+
+export default Header
