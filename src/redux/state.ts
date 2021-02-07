6ea1fde28f75aa1c75c95c2bb2fe64a8ca77fb66
dimrys <akkauntdlyraboti@gmail.com>
2021-02-07 14:19:48 +0300

    add store

--- src/redux/state.ts
+++ src/redux/state.ts
@@ -37,12 +37,95 @@ export type State1Type = {
     sidebar: SidebarType
 }
 
+
+type StoreType = {
+    _state: State1Type
+    _changeState: () => void
+    subscribe: (observer: () => void) => void
+    addPost: () => void
+    updateNewPost: (textInAdPost: string) => void
+    addMessage: () => void
+    updateNewMessage: (textInAdMessage: string) => void
+    getState: () => State1Type
+
+
+}
+
+
+const store: StoreType = {
+    _state: {
+        profilePage: {
+            posts: [
+                {id: 1, message: "Hello, world!", likes: 5},
+                {id: 2, message: "I like programming", likes: 12},
+                {id: 3, message: "What is your name?", likes: 122},
+            ],
+            newPostText: ''
+
+        },
+        dialogsPage: {
+            dialogs: [
+                {id: 1, name: "Dima"},
+                {id: 2, name: "Sasha"},
+                {id: 3, name: "Sasha"},
+                {id: 4, name: "Masha"},
+                {id: 5, name: "Kolya"}
+            ],
+            messages: [
+                {id: 1, message: "Hi"},
+                {id: 2, message: "How are you ?"},
+                {id: 3, message: "I am fine"},
+                {id: 4, message: "Do you speak english"},
+                {id: 5, message: "YOOO"}
+            ],
+            newMessageText: ''
+        },
+        sidebar: {}
+    },
+    _changeState() {
+        console.log("state chenged")
+    },
+    subscribe(observer) {
+        this._changeState = observer
+    },
+    addPost() {
+        const newPost: PostType = {
+            id: new Date().getDate(), message: this._state.profilePage.newPostText, likes: 0
+        }
+        this._state.profilePage.posts.push(newPost)
+        this._state.profilePage.newPostText = ''
+        this._changeState()
+    },
+    updateNewPost(textInAdPost) {
+        this._state.profilePage.newPostText = textInAdPost
+        this._changeState()
+    },
+    addMessage() {
+        const newMessage: MessageType = {
+            id: new Date().getDate(), message: this._state.dialogsPage.newMessageText
+        }
+        this._state.dialogsPage.messages.push(newMessage)
+        this._state.dialogsPage.newMessageText = ''
+        this._changeState()
+    },
+    updateNewMessage(textInAdMessage) {
+        this._state.dialogsPage.newMessageText = textInAdMessage
+        this._changeState()
+    },
+    getState() {
+        const state = this._state
+        return state
+    }
+
+
+}
+
 export const state1: State1Type = {
     profilePage: {
         posts: [
-            {id: 1, message: "Hello, world!", likes: 5 },
-            {id: 2, message: "I like programming", likes: 12 },
-            {id: 3, message: "What is your name?", likes: 122 },
+            {id: 1, message: "Hello, world!", likes: 5},
+            {id: 2, message: "I like programming", likes: 12},
+            {id: 3, message: "What is your name?", likes: 122},
         ],
         newPostText: ''
 
@@ -69,7 +152,7 @@ export const state1: State1Type = {
 
 export const addPost = () => {
     const newPost: PostType = {
-        id:new Date().getDate(), message: state1.profilePage.newPostText, likes: 0
+        id: new Date().getDate(), message: state1.profilePage.newPostText, likes: 0
     }
     state1.profilePage.posts.push(newPost)
     state1.profilePage.newPostText = ''
